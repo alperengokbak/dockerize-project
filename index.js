c8d6989e557b18c28d49bd018fd0f728d64f3a4b
Alperen Gökbak <swalperen3008@gmail.com>
2024-05-18 03:35:56 +0300

    Test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get("/helloworld", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
